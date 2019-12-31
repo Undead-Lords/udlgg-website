36f3424996f0964196496c853519bb8747339d42
TheScree <screelings@gmail.com>
2019-12-31 16:41:40 -0600

    Minor Location Updates & Redirect Update

--- pages/_id.vue
+++ pages/_id.vue
@@ -19,16 +19,16 @@
         <section class="px-2 lg:px-0 mt-16 sm:mt-32 flex w-full container mx-auto pb-16">
             <div class="w-full text-white flex justify-center md:justify-start text-center md:text-left">
                 <div class="md:w-3/4 w-full">
-                  <h2 class="mb-8 leading-none font-bold text-2xl xs:text-2x1 md:text-4xl lg:5x1 uppercase center">Harvesting Souls Since <span class="text-red-800">1994</span></h2>
+                  <h2 class="mb-8 leading-none font-bold text-2xl xs:text-2x1 md:text-4xl lg:5x1 uppercase center">Harvesting Souls Since <span class="text-red-800t">1994</span></h2>
                   <div class="udl-shroud rounded-lg p-5">
-                    <p class="m-3">You are travelling through Myrkul's domain. You will be teleported to your destination shortly.</p>
-                    <p class="m-3">Your selected destination is <a :href="shortlink.udl_destination" class="text-red-800 font-bold">{{ shortlink.udl_destination }}</a> and has been visited by <span class="text-red-800 font-bold">{{ shortlink.udl_visitors }}</span> travelers before you. The last traveler used this path on <span class="text-red-800 font-bold">{{ shortlink.udl_last_visited }}</span>.</p>
-                    <p class="m-3">This route was originally harvested by <span class="text-red-800 font-bold">{{ shortlink.udl_creator }}</span> on <span class="text-red-800 font-bold">{{ shortlink.udl_created }}</span>.</p>
-                  </div>
-                </div>
-                <div class="md:w-1/4 md:justify-center md:flex hidden">
-                    <img src="images/udl-logo.png" class="w-full" alt="Undead Lords Skull Logo" />
-                </div>
+                    <p class="m-3">You are travelling through Myrkul's domain. You will be teleported to your destination in <span class="text-red-800 bold">6</span> seconds.</p>
+                    <p class="m-3">Your selected destination is <a :href="shortlink.udl_destination" class="text-red-800 font-bold">{{ shortlink.udl_destination }}</a> and has been visited by <span class="text-red-800 font-bold">{{ shortlink.udl_visitors }}</span> travelers before you. The last traveler used this path <span class="text-red-800 font-bold">{{ last_visited }}</span>.</p>
+                    <p class="m-3">This route was originally harvested by <span class="font-bold">{{ shortlink.udl_creator }}</span> <span class="text-red-800 font-bold">{{ created_on }}.</span>
+                 </div>
+               </div>
+              <div class="md:w-1/4 md:justify-center md:flex hidden">
+                  <img src="images/udl-logo.png" class="w-full" alt="Undead Lords Skull Logo" />
+              </div>
             </div>
         </section>
       </div>
@@ -37,18 +37,26 @@
 </template>
 
 <script>
+import moment from 'moment';
+
 export default {
  async asyncData({ app, params, error }) {
    return app.$axios.$get(`https://api.udl.gg/${params.id}`)
      .then((res) => {
         return {
           shortlink: res,
-          destination: res.udl_destination
+          destination: res.udl_destination,
+          last_visited: moment(res.udl_last_visited).fromNow(),
+          created_on: moment(res.udl_created).fromNow()
         }
      })
-     .catch(e=>{
+     .catch(e => {
        console.log(e.stack);
-       return { destination: 'https://undeadlords.net'}
+       return {
+         destination: 'https://undeadlords.net',
+         last_visited: 0,
+         created_on: 0
+       }
      })
    },
    head () {
@@ -56,7 +64,7 @@ export default {
       title: 'Link Shortener',
       meta: [
         { charset: 'utf-8' },
-        { "http-equiv": 'Refresh', content: `20; url=${this.destination}`}
+        { "http-equiv": 'Refresh', content: `6; url=${this.destination}`}
       ]
     }
   }
@@ -64,7 +72,7 @@ export default {
 </script>
 <style>
 .udl-shroud {
-  background: rgba(0, 0, 0, .65);
-  backdrop-filter: blur(3px);
+  background: rgba(0, 0, 0, .80);
+  backdrop-filter: blur(2px);
 }
 </style>
